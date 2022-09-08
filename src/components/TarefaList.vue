ec61751c5871f2f51dafcf69b1f7c57e0e5f4789
evandropcs <evandropcs@hotmail.com>
2022-09-08 14:56:57 -0300

    primeira versão todo

---
+++ src/components/TarefaList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="tarefa in tasks" :key="tarefa.id">{{ tarefa }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+    tasks: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
