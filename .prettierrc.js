915ff485501aeafd16fc88d7f876788ef3526162
evandropcs <evandropcs@hotmail.com>
2022-09-09 11:07:15 -0300

    adiciona botões adicionar, salvar, e comportamento da apimock

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, //ponto e virgula
+  singleQuote: false, //aspas duplas
+  tabWidth: 2, // dois espaços no tab
+  useTabs: false, //
+  trailingComma: "es5",
+  arrowParens: "always", // parentese opcional no arrowfunction
+}
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,15 +1,45 @@
 <template>
   <div id="app">
     <nav class="orange darken-2">
-      <div class="nav-wrapper"></div>
+      <div class="nav-wrapper">
+        <img alt="Vue logo" src="../assets/logo.png" width="32" />
+      </div>
     </nav>
-    <img alt="Vue logo" src="../assets/logo.png" width="32" />
-    <tarefa-list :msg="'Welcome to Your Vue.js App'" :tasks="listaDeTarefa" />
+
+    <div style="padding: 20px" v-show="exibir.lista">
+      <button class="btn" @click="mostrarCadastro">Adicionar</button>
+    </div>
+
+    <!-- Lista -->
+    <div v-show="exibir.lista">
+      <TarefaList :msg="'Welcome to Your Vue.js App'" :tasks="listaDeTarefa" />
+    </div>
+    <!-- Form -->
+    <div v-show="exibir.form">
+      <div style="padding: 20px">
+        <button class="btn" @click="salvarTarefa">Salvar</button>
+      </div>
+      <h2>Cadastrar Tarefas</h2>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Entre com a tarefa"
+        v-model="form.title"
+      />
+      <input
+        type="text"
+        name="project"
+        id="project"
+        placeholder="project"
+        v-model="form.project"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import TaskApi from "../TasksApi";
+import TasksApi from "../TasksApi.js";
 import TarefaList from "../components/TarefaList.vue";
 
 export default {
@@ -18,13 +48,41 @@ export default {
   },
   data: () => {
     return {
-      listaDeTarefa: ["A", "B", "C"],
+      listaDeTarefa: [],
+      exibir: {
+        lista: true,
+        form: false,
+      },
+      form: {
+        title: "",
+        project: "",
+      },
     };
   },
+
+  methods: {
+    listarTarefas() {
+      TasksApi.getTasks((data) => {
+        this.listaDeTarefa = data;
+      });
+    },
+    mostrarCadastro() {
+      (this.exibir.form = true), (this.exibir.lista = false);
+    },
+    salvarTarefa() {
+      (this.exibir.form = false), (this.exibir.lista = true);
+      const novaTarefa = {
+        title: this.form.title,
+        project: this.form.project,
+        date: new Date().toLocaleDateString("pt"),
+      };
+      TasksApi.createTask(novaTarefa, () => {
+        this.listarTarefas();
+      });
+    },
+  },
   created() {
-    TaskApi.getTasks((data) => {
-      this.listaDeTarefa = data;
-    });
+    this.listarTarefas();
   },
 };
 </script>
